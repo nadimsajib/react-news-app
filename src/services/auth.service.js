ab58432c5e3f7d20dbc2bd740fad70d98c239cd7
Nadim <nhsajib316@gmail.com>
2023-07-04 01:29:07 +0600

    first commit

---
+++ src/services/auth.service.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8000/api/";
+
+const register = (name, email, password) => {
+  return axios.post(API_URL + "register", {
+    name,
+    email,
+    password,
+  });
+};
+
+const login = (email, password) => {
+  return axios
+    .post(API_URL + "login", {
+      email,
+      password,
+    })
+    .then((response) => {
+      if (response.data.access_token) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+      return response.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+};
+
+export default {
+  register,
+  login,
+  logout,
+};
